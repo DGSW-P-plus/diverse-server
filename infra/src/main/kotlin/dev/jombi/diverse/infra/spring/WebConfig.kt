a9c99a355da65a843615a603ac4c0d3e0dc9e878
jombi <jombi@duck.com>
2024-07-16 20:13:54 +0900

    feat :: configures

---
+++ infra/src/main/kotlin/dev/jombi/diverse/infra/spring/WebConfig.kt
@@ -0,0 +1,15 @@
+package dev.jombi.diverse.infra.spring
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry
+            .addResourceHandler("/static/**")
+            .addResourceLocations("classpath:/static/")
+            .setCachePeriod(30)
+    }
+}
