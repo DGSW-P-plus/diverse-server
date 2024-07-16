390ac13e638956d91de84a240d4ab5cb16853d9b
전민오 <jbj338033@gmail.com>
2024-07-16 17:17:20 +0900

    feat :: querydsl

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,12 @@
 dependencies {
     api("org.springframework.boot:spring-boot-starter-data-mongodb")
     api("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    api("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
     api(project(":business"))
     api(project(":common"))
 }
