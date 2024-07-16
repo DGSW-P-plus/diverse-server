3244b1654736752ba174128a5606034236c2ce7a
jombi <jombi@duck.com>
2024-07-16 21:14:24 +0900

    fix :: unused MemberId

--- core/src/main/kotlin/dev/jombi/diverse/core/member/entity/MemberId.kt
+++
@@ -1,9 +0,0 @@
-package dev.jombi.diverse.core.member.entity
-
-import dev.jombi.diverse.core.member.repository.MemberJpaRepository
-import org.springframework.data.repository.findByIdOrNull
-
-@JvmInline
-value class MemberId(val id: Long) {
-    fun fetch(repository: MemberJpaRepository) = repository.findByIdOrNull(id)
-}
