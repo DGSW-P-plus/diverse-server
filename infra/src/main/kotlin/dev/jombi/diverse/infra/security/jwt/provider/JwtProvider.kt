8bd3d51ff15d8267ebc8be93f5db30e1a071f5b6
전민오 <jbj338033@gmail.com>
2024-07-17 12:35:28 +0900

    fix :: chat

--- infra/src/main/kotlin/dev/jombi/diverse/infra/security/jwt/provider/JwtProvider.kt
+++ infra/src/main/kotlin/dev/jombi/diverse/infra/security/jwt/provider/JwtProvider.kt
@@ -2,21 +2,15 @@ package dev.jombi.diverse.infra.security.jwt.provider
 
 import dev.jombi.diverse.common.exception.CustomException
 import dev.jombi.diverse.common.exception.GlobalExceptionDetail
-import dev.jombi.diverse.infra.security.details.MemberDetailsService
 import dev.jombi.diverse.infra.security.jwt.JwtAuthToken
-import dev.jombi.diverse.infra.security.jwt.JwtProperties
 import dev.jombi.diverse.infra.security.jwt.TokenValidator
-import io.jsonwebtoken.Jwts
 import org.springframework.security.authentication.AuthenticationProvider
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
 
 @Component
 class JwtProvider(
     private val validator: TokenValidator,
-    private val jwtProperties: JwtProperties,
-    private val memberDetailsService: MemberDetailsService
 ) : AuthenticationProvider {
     override fun authenticate(authentication: Authentication?): Authentication {
         val jwt = authentication?.credentials?.toString()
@@ -27,12 +21,5 @@ class JwtProvider(
 
     override fun supports(authentication: Class<*>): Boolean {
         return JwtAuthToken::class.java == authentication
-    }
-
-    fun getAuthentication(token: String): Authentication {
-        val username = Jwts.parser().verifyWith(jwtProperties.secretKey()).build().parseSignedClaims(token).payload.subject
-        val details = memberDetailsService.loadUserByUsername(username)
-
-        return UsernamePasswordAuthenticationToken(details, null, details.authorities)
     }
 }
