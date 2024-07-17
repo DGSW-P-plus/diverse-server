93748235d82df895e716ad51be55ab0525223057
jombi <jombi@duck.com>
2024-07-17 16:29:56 +0900

    fix :: set my gender error

--- core/src/main/kotlin/dev/jombi/diverse/core/gender/domain/entity/MemberGender.kt
+++ core/src/main/kotlin/dev/jombi/diverse/core/gender/domain/entity/MemberGender.kt
@@ -6,12 +6,12 @@ import jakarta.persistence.*
 @Entity(name = "tb_gender_member")
 class MemberGender(
     @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("_genderId")
+    @MapsId("genderId")
     @JoinColumn(name = "gender_id", referencedColumnName = "id")
     val gender: Gender,
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("_memberId")
+    @MapsId("memberId")
     @JoinColumn(name = "member_id", referencedColumnName = "id")
     val member: Member,
 
