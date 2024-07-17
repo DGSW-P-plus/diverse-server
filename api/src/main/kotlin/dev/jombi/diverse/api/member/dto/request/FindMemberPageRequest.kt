748e1cd1908bdd175b5e4d98a4569d8e79ab794e
jombi <jombi@duck.com>
2024-07-17 19:49:34 +0900

    feat :: Member find controller

---
+++ api/src/main/kotlin/dev/jombi/diverse/api/member/dto/request/FindMemberPageRequest.kt
@@ -0,0 +1,6 @@
+package dev.jombi.diverse.api.member.dto.request
+
+data class FindMemberPageRequest(
+    val size: Long,
+    val page: Long
+)
--- api/src/main/kotlin/dev/jombi/diverse/api/member/presentation/MemberController.kt
+++ api/src/main/kotlin/dev/jombi/diverse/api/member/presentation/MemberController.kt
@@ -1,6 +1,7 @@
 package dev.jombi.diverse.api.member.presentation
 
 import dev.jombi.diverse.api.member.dto.MemberDtoMapper
+import dev.jombi.diverse.api.member.dto.request.FindMemberPageRequest
 import dev.jombi.diverse.api.member.dto.request.MemberEditOptionalRequest
 import dev.jombi.diverse.api.member.dto.response.MemberInfoResponse
 import dev.jombi.diverse.business.member.service.MemberService
@@ -9,11 +10,7 @@ import dev.jombi.diverse.common.response.ResponseEmpty
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Member", description = "멤버")
 @RestController
@@ -33,5 +30,10 @@ class MemberController(
     fun editMyInfo(@RequestBody @Valid request: MemberEditOptionalRequest): ResponseEntity<ResponseEmpty> {
         memberService.editMyInfo(memberDtoMapper.convertToDto(request))
         return ResponseEmpty.ok()
+    }
+
+    @GetMapping("/find")
+    fun findFriend(@ModelAttribute pageRequest: FindMemberPageRequest) {
+
     }
 }
