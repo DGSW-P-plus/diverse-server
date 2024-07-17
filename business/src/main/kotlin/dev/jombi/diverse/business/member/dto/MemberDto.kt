d21d37c5f9488e4da94ea567e71e944c62187fb9
jombi <jombi@duck.com>
2024-07-18 00:42:59 +0900

    feat :: sns fetch on user detail fetch

--- business/src/main/kotlin/dev/jombi/diverse/business/member/dto/MemberDto.kt
+++ business/src/main/kotlin/dev/jombi/diverse/business/member/dto/MemberDto.kt
@@ -1,6 +1,7 @@
 package dev.jombi.diverse.business.member.dto
 
 import dev.jombi.diverse.business.gender.dto.GenderDto
+import dev.jombi.diverse.business.sns.dto.SNSDto
 
 data class MemberDto(
     val id: Long,
@@ -8,5 +9,6 @@ data class MemberDto(
     val nickname: String,
     val location: String?,
     val bio: String?,
-    val genders: List<GenderDto>
+    val genders: List<GenderDto>,
+    val sns: List<SNSDto>
 )
