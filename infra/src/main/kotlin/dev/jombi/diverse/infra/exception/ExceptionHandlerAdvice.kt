3c082b682e7116a87c98439c6e4467ec18ee27b5
jombi <jombi@duck.com>
2024-07-17 13:18:06 +0900

    fix :: Valid exception handle

--- infra/src/main/kotlin/dev/jombi/diverse/infra/exception/ExceptionHandlerAdvice.kt
+++ infra/src/main/kotlin/dev/jombi/diverse/infra/exception/ExceptionHandlerAdvice.kt
@@ -8,8 +8,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.validation.FieldError
 import org.springframework.web.HttpMediaTypeNotSupportedException
 import org.springframework.web.HttpRequestMethodNotSupportedException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -68,4 +70,11 @@ class ExceptionHandlerAdvice {
     @ExceptionHandler
     fun badCredentialsException(e: BadCredentialsException) =
         ResponseError.of(AuthExceptionDetails.BAD_CREDENTIALS)
+
+    @ExceptionHandler
+    fun methodArgumentNotValidException(e: MethodArgumentNotValidException) =
+        ResponseError.of(
+            GlobalExceptionDetail.INVALID_PARAMETER,
+            e.bindingResult.allErrors.joinToString(", ") { "in field '${(it as FieldError).field}': ${it.defaultMessage}" }
+        )
 }
