38f2aa3da3d39aa0801e730d73bbc608f6324174
jombi <jombi@duck.com>
2024-07-18 01:46:51 +0900

    chore :: global pagable

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
     api("org.springframework.boot:spring-boot-starter-data-mongodb")
-    api("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
 
     api("com.querydsl:querydsl-core")
