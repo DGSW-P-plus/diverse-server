50da10ffc76cd9a3fc14174fef7f4e1f765251c3
jombi <jombi@duck.com>
2024-07-17 12:30:33 +0900

    feat :: readOnly transaction

--- core/src/main/kotlin/dev/jombi/diverse/core/gender/service/GenderServiceImpl.kt
+++ core/src/main/kotlin/dev/jombi/diverse/core/gender/service/GenderServiceImpl.kt
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class GenderServiceImpl(
     private val genderJpaRepository: GenderJpaRepository,
     private val memberGenderJpaRepository: MemberGenderJpaRepository,
